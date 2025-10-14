d604201935a061e37a0af68750939e3b231fe15b
vivek02-xo <vivek02.xo@gmail.com>
2025-10-14 18:40:31 +0530

    UPDATE

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', async (req, res) => {
-    res.send('Hello World');
+    res.render("listings/home.ejs");
 });
 
 // Index Route 
